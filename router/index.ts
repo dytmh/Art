2f65ac7ede588ecd3f57f47e9e2ca6eb95982cdd
dyt <dytmh@163.com>
2024-08-13 21:44:43 +0800

    1

--- router/index.ts
+++ router/index.ts
@@ -9,10 +9,4 @@ router.get('/api/wx_openid', userApi.wx_openid)
 router.post('/api/login', userApi.login)
 router.get('/api/login', userApi.login)
 
-router.post('/api/logout', userApi.logout)
-router.get('/api/logout', userApi.logout)
-
-router.post('/api/checkSession', userApi.checkSession)
-router.get('/api/checkSession', userApi.checkSession)
-
 export default router
--- routes/user.ts
+++ routes/user.ts
@@ -14,63 +14,11 @@ namespace userApi
 
     export const login: HandleHttpApi = async (req, res) => {
         console.log(req.headers)
-
-        const wxRes = await axios.request({
-            url: 'https://api.weixin.qq.com/sns/jscode2session',
-            method: 'GET',
-            responseType: 'json',
-            params: {
-                'appid': GlobalConfig.AppId,
-                'secret': GlobalConfig.AppSecret,
-                'js_code': req.body["code"] ?? req.query["code"],
-                'grant_type': 'authorization_code'
-            }
-        })
-
-        console.log(wxRes.data)
-
-        if (wxRes.data && !isNullOrEmpty(wxRes.data.openid)) {
-        }
+        console.log(req.body)
         
-        res.send({
-            errcode: wxRes.data.errcode,
-            errmsg: wxRes.data.errmsg
-        })
-    }
-
-    export const logout: HandleHttpApi = async (req, res) => {
-        const wxRes = await axios.request({
-            url: 'https://api.weixin.qq.com/wxa/resetusersessionkey',
-            method: 'GET',
-            responseType: 'json',
-            params: {
-                'openid': req.headers["userid"] ?? req.headers["userid"],
-                'signature': CryptoJS.HmacSHA256(req.headers["token"] as string, "").toString(),
-                'sig_method': 'hmac_sha256'
-            }
-        })
-
         res.send({
             errcode: 0,
             errmsg: ''
-        })
-    }
-
-    export const checkSession: HandleHttpApi = async (req, res) => {
-        const wxRes = await axios.request({
-            url: 'https://api.weixin.qq.com/wxa/checksession',
-            method: 'GET',
-            responseType: 'json',
-            params: {
-                'openid': req.headers["userid"] ?? req.headers["userid"],
-                'signature': CryptoJS.HmacSHA256(req.headers["token"] as string, "").toString(),
-                'sig_method': 'hmac_sha256'
-            }
-        })
-
-        res.send({
-            errcode: wxRes.data.errcode,
-            errmsg: wxRes.data.errmsg
         })
     }
 }
